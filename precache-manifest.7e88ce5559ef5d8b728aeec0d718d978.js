614342bc922a9bab4372cc07daaaa26448d42258
Jesse Spaulding <spauld81@gmail.com>
2020-05-09 16:25:38 -0500

    Updates

---
+++ precache-manifest.7e88ce5559ef5d8b728aeec0d718d978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddb1a8d4d2d75b206ddf93cf3e4c1bd",
+    "url": "/coronavirus-super-spreader-model/index.html"
+  },
+  {
+    "revision": "0004533b3d8ee521def5",
+    "url": "/coronavirus-super-spreader-model/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bfa171a5f4ddd2ca522b",
+    "url": "/coronavirus-super-spreader-model/static/css/main.f5ad9fb4.chunk.css"
+  },
+  {
+    "revision": "0004533b3d8ee521def5",
+    "url": "/coronavirus-super-spreader-model/static/js/2.faa78602.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coronavirus-super-spreader-model/static/js/2.faa78602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa171a5f4ddd2ca522b",
+    "url": "/coronavirus-super-spreader-model/static/js/main.49f3e8e2.chunk.js"
+  },
+  {
+    "revision": "2c9cd357708ec878ee6e",
+    "url": "/coronavirus-super-spreader-model/static/js/runtime-main.5c7f2dc3.js"
+  }
+]);
